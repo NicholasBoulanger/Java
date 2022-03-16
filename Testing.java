3b62c6a256086ae14048034fa84acc2261208da6
NicholasBoulanger <101681695+NicholasBoulanger@users.noreply.github.com>
2022-03-15 18:12:26 -0700

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,49 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+
+public class Testing {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Commented Out
+		
+		System.out.println("Hello");
+		System.out.println("Test");
+		
+		//Scanner class to accept input
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("What is your name? \nEnter below: \n");
+		String input = sc.nextLine();
+		
+		System.out.println("Hello " + input);
+		
+
+		try
+		{
+		    Thread.sleep(10000);
+		}
+		catch(InterruptedException ex)
+		{
+		    Thread.currentThread().interrupt();
+		}
+		
+		
+		System.out.println("Thank you for waiting" + input);
+		
+		
+		
+		
+		
+	}
+
+}
